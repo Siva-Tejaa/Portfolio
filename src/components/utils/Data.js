b7392d0b95e20a5e90d1f24135dca27e268b9e6d
Siva-Tejaa <94619963+Siva-Tejaa@users.noreply.github.com>
2023-03-12 16:17:16 +0530

    1 file modified

--- src/components/utils/Data.js
+++ src/components/utils/Data.js
@@ -64,6 +64,14 @@ export const projectsData = [
     category: "react",
     demo: "https://sivateja.stackblitz.io",
     repo: ""
+  },
+  {
+    id: 9,
+    image: "https://raw.githubusercontent.com/Siva-Tejaa/Projects-Data/main/PasswordGenerator.png",
+    title: "Password Generator",
+    category: "react",
+    demo: "https://react-password-generator.stackblitz.io",
+    repo: ""
   }
 
 
